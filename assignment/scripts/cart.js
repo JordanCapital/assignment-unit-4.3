cb633b0cd6d701bd0c269fd8dfa668db040040fd
Jordan Capital <118566815+JordanCapital@users.noreply.github.com>
2023-02-13 00:02:56 +0300

    Update cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// empty array basket
+var basket = [];
+const maxItems = 5;
+
+// function addItem
+function addItem(item){
+    basket.push(item);
+    return true;
+}
